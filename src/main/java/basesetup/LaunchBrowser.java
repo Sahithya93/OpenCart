68ede36f5068c8ec1a9b82e7a24c6540c8fef611
Sahithya93 <36257098+Sahithya93@users.noreply.github.com>
2018-02-20 11:02:50 +0530

    changes in launchbrowser

--- src/main/java/basesetup/LaunchBrowser.java
+++ src/main/java/basesetup/LaunchBrowser.java
@@ -1,11 +1,15 @@
 package basesetup;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.Reporter;
@@ -15,21 +19,23 @@ import org.testng.annotations.Test;
 
 public class LaunchBrowser {
 	static WebDriver driver;
+	static DesiredCapabilities capability;
 	
-	
-	public static WebDriver OpenBrowser(String Browser){
+	public static WebDriver OpenBrowser(String Browser) throws Exception{
 	
 	if(Browser.equalsIgnoreCase("chrome")){
-	System.setProperty("webdriver.chrome.driver", "D:\\Selenium Stuff\\Selenium Jars\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("http://10.207.182.108:81/opencart/");
-	driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-	}
-	return driver;
-	}
+		
+		capability = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://10.159.34.141:4444/wd/hub"), capability);
+        driver.get("http://10.207.182.108:81/opencart");
+        
+        
+	
 	/*
 	
 	
 	}*/
+}
+	return driver;
+	}
 }
